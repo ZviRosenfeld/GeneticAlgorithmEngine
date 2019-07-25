2df95a8dd9b2ea386bcec9dcf3b4a04a8e7e89ec
Zvi Rosenfeld <Zvi.Rosenfeld@checkmarx.com>
2019-07-25 10:39:17 +0300

    Added some tests

--- GeneticAlgorithm.UnitTests/SearchTests.cs
+++ GeneticAlgorithm.UnitTests/SearchTests.cs
@@ -45,5 +45,51 @@ namespace GeneticAlgorithm.UnitTests
             Assert.AreEqual(10, maxEvaluation);
             Assert.AreEqual(eilentPrecentage * populationSize, result.Population.Count(c => c.Evaluate() == maxEvaluation));
         }
+
+        [TestMethod]
+        public void SearchTimeTest()
+        {
+            var populationManager = new TestPopulationManager( new double[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
+            var engine1 = new TestGeneticSearchEngineBuilder(10, 10, populationManager).Build();
+            var engine2 = new TestGeneticSearchEngineBuilder(10, 1000, populationManager).Build();
+
+            var result1 = engine1.Search();
+            var result2 = engine2.Search();
+
+            Assert.IsTrue(result2.SearchTime > result1.SearchTime, $"engine1 ran for less time than engine2 (engine1 = {result1.SearchTime}; engine2 = {result2.SearchTime})");
+        }
+
+        [DataRow(false)]
+        [DataRow(true)]
+        [TestMethod]
+        public void HistoryExistTest(bool includeHistory)
+        {
+            var populationManager = new TestPopulationManager(new double[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
+            var enigneBuilder = new TestGeneticSearchEngineBuilder(10, 2, populationManager);
+            var searchEngine = includeHistory ? enigneBuilder.IncludeAllHistory().Build() : enigneBuilder.Build();
+
+            var result = searchEngine.Search();
+
+            if (includeHistory)
+                Assert.AreEqual(3, result.History.Count, "There should have been history");
+            else
+                Assert.AreEqual(0, result.History.Count, "There shouldn't be any history");
+        }
+
+        [TestMethod]
+        public void HistoryIsRightTest()
+        {
+            var population = new[] {new double[] {1, 1, 1}, new double[] {2, 2, 2}, new double[] {2, 3, 2}};
+            var populationManager = new TestPopulationManager(population);
+            var searchEngine =
+                new TestGeneticSearchEngineBuilder(population[0].Length, population.Length - 1, populationManager)
+                    .IncludeAllHistory().Build();
+
+            var result = searchEngine.Search();
+
+            for (int i = 0; i < population.Length; i++)
+            for (int j = 0; j < population[i].Length; j++)
+                Assert.AreEqual(population[i][j], result.History[i][j].Evaluate());
+        }
     }
 }
--- GreatestVectorTests/BassicTests.cs
+++ GreatestVectorTests/BassicTests.cs
@@ -30,24 +30,5 @@ namespace GreatestVectorTests
             Assert.IsTrue(NumberVectorBassicPopulationGenerator.VECTOR_SIZE < result.BestChromosome.Evaluate(),
                 $"best result ({result.BestChromosome.Evaluate()}) should have been greater than {NumberVectorBassicPopulationGenerator.VECTOR_SIZE}");
         }
-
-        [DataRow(false)]
-        [DataRow(true)]
-        [TestMethod]
-        public void HistoryTest(bool includeHistory)
-        {
-            var enigneBuilder = new GeneticSearchEngineBuilder(POPULATION_SIZE, 2, new NumberVectorCrossoverManager(),
-                new NumberVectorBassicPopulationGenerator());
-            if (includeHistory)
-                enigneBuilder = enigneBuilder.IncludeAllHistory();
-            var searchEngine = enigneBuilder.Build();
-
-            var result = searchEngine.Search();
-
-            if (includeHistory)
-                Assert.AreEqual(3, result.History.Count, "There should have been history");
-            else
-                Assert.AreEqual(0, result.History.Count, "There shouldn't be any history");
-        }
     }
 }
